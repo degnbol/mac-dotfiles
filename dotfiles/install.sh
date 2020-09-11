a4cce9810ce22179b29ddf9a0ec1e0349f599614
Christian Degnbol Madsen <cdegma@biosustain.dtu.dk>
2020-09-11 15:34:11 +0200

    inital commit

---
+++ dotfiles/install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env zsh
+# make zsh default shell
+chsh -s $(which zsh)
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install exa curl wget htop cmake
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install antibody
+brew install getantibody/tap/antibody
+
+# run this when you want to update plugins
+antibody bundle < ~/dotfiles/zsh_plugins.txt > ~/dotfiles/zsh_plugins.sh
+
+# neovim
+brew install python neovim
+
+# install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# open vi and run :PlugInstall
+# there will be an issue with youcompleteme. 
+# It has to be installed with non-conda python3 where both non-conda and conda pythons should have access to pynvim, 
+# which can be checked by running import pynvim inside python3.
+conda install pynvim -c conda-forge
+conda deactivate
+# pip3 should be installed as a part of the brew python, which is the python we will use here to install youcompleteme
+pip3 install pynvim
+python3 ~/.config/nvim/plugged/youcompleteme/install.py 
+vi  # check that it works (there should be no error saying ycm shutdown)
+conda activate  # go back to normal
+vi # check that it also works here
+
+brew install rename
+
+# python
+conda install numpy scipy pandas ipython biopython
+
+
+# install R from https://cran.r-project.org/bin/macosx/ then
+brew install llvm
+# install X11 support from https://www.xquartz.org/ and restart laptop then
+R
+# there has been many issues with data.table which they blame on apple.
+install.packages("data.table", type="source", repos="https://Rdatatable.gitlab.io/data.table")
+install.packages(c("ggplot2", "Matrix", "tidyverse", "BiocManager"))
+# from bioconductor
+library(BiocManager)
+install(c("DOSE", "fgsea"))  # Gene Set Enrichment Analysis
+
+
---
+++ dotfiles/zsh_plugins.sh
@@ -0,0 +1,13 @@
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages )
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /Users/cdegma/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
