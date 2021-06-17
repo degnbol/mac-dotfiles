550263d804a6f7be3d7650aadc2c752b2eb72643
Christian Madsen <christian.degnbol@gmail.com>
2021-06-17 14:46:51 +0200

    color tweaks

--- .config/nvim/lua/top-bufferline.lua
+++ .config/nvim/lua/top-bufferline.lua
@@ -6,14 +6,14 @@ require "bufferline".setup {
         close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
-        max_name_length = 14,
-        max_prefix_length = 13,
+        max_name_length = 20,
+        max_prefix_length = 15,
         tab_size = 20,
-        show_tab_indicators = true,
-        enforce_regular_tabs = false,
+        show_tab_indicators = false,
+        enforce_regular_tabs = false, -- enforce same size tabs
         view = "multiwindow",
-        show_buffer_close_icons = true,
-        separator_style = "thin",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
         mappings = "true"
     }
 }
