7e31586c921fe26167da9cc3595fd7fb07c42ced
Christian Madsen <christian.degnbol@gmail.com>
2021-06-25 00:04:31 +0200

    italics in term

---
+++ fonts/italics/term_italics.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# https://weibeld.net/terminals-and-shells/italics.html
+# test italics with this command:
+echo `tput sitm`italics`tput ritm`
+# if it doesn't show italics then try this:
+tic terminfo
+
