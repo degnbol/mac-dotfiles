2c57586b4a564a8e43b8f5ae1d437f08e91da63d
Christian Madsen <christian.degnbol@gmail.com>
2021-07-19 11:47:05 +0200

    folding not yet working from treesitter

--- config/nvim/lua/treesitter-nvim.lua
+++ config/nvim/lua/treesitter-nvim.lua
@@ -16,3 +16,10 @@ ts_config.setup {
         use_languagetree = true
     }
 }
+
+
+vim.wo.foldmethod = 'indent'
+vim.wo.foldlevel = 99 -- so we don't fold from the start
+-- I would like to use treesitter folding but treesitter doesn't work if I write the two lines as instructed on their readme
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
